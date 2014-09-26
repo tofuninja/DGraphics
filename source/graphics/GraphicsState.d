41f9a6d96eb84b04aba681be07a99f8a6ebe03d2
Joseph Emmons <joeyemmons@yahoo.com>
2014-09-26 05:45:36 -0400

    Move setup into graphics init
    Glfw window setup, derilict setup and other setup all moved into
    graphics state init

--- source/graphics/GraphicsState.d
+++ source/graphics/GraphicsState.d
@@ -1,16 +1,20 @@
 ﻿module graphics.GraphicsState;
 // Global Graphics State
 
+import derelict.glfw3.glfw3;
 import derelict.opengl3.gl3;
-
-
-
+import derelict.opengl3.gl;
+import derelict.freeimage.freeimage;
+import std.stdio; 
+import gui.Panel;
 
 // State
 private int texUnitCount;
-
-
-
+public TextureImageUnit[] TextureBindPoints;
+public GLFWwindow* window;
+public enum windowW = 900;
+public enum windowH = 500;
+public BasePanel basePan;
 
 
 /**
@@ -18,6 +22,35 @@ private int texUnitCount;
  */
 public void initializeGraphicsState()
 {
+	/* Initialize the librarys */
+	DerelictGL.load();
+	DerelictGL3.load();
+	DerelictGLFW3.load();
+	DerelictFI.load();
+	
+	if (!glfwInit()) return;
+	
+	// Create a windowed mode window and its OpenGL context 
+
+	glfwWindowHint(GLFW_RESIZABLE, 0);
+	window = glfwCreateWindow(windowW, windowH, "Hello World", null, null);
+	//scope(exit) glfwTerminate();
+	if (!window) return;
+	glfwMakeContextCurrent(window);
+	DerelictGL3.reload();
+	DerelictGL.reload();
+
+	debug writeln("OpenGl Version:", DerelictGL3.loadedVersion);
+	/*
+	// Enforce required gl
+	{
+		import std.exception;
+		enforce(DerelictGL3.loadedVersion >= GLVersion.GL40, "Min Gl version is 4.0");
+		enforce(ARB_program_interface_query, "Requires either ARB_program_interface_query or Gl version 4.3");
+		enforce(ARB_separate_shader_objects, "Requires either ARB_separate_shader_objects or Gl version 4.1");
+	}
+
+
 	glGetIntegerv(GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS, &texUnitCount);
 
 	TextureBindPoints = new TextureImageUnit[texUnitCount];
@@ -26,11 +59,15 @@ public void initializeGraphicsState()
 		TextureBindPoints[i] = TextureImageUnit(i);
 	}
 
-	debug
-	{
-		import std.stdio;
-		writeln("Texture Unit Count: ", texUnitCount);
+	debug writeln("Texture Unit Count: ", texUnitCount);
+	*/
+
+	{ // Load font file
+		import gui.Font;
+		initFont();
 	}
+
+	basePan = new BasePanel(windowW,windowH);
 }
 
 /**
@@ -66,7 +103,3 @@ public struct TextureImageUnit
 	}
 }
 
-/**
- * Texture Bind Points
- */
-public TextureImageUnit[] TextureBindPoints;
