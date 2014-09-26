5afaf5b7eb33667cb2b96655483fd6ab324f94e2
Joseph Emmons <joeyemmons@yahoo.com>
2014-09-26 05:46:06 -0400

    Simple 3d camera

---
+++ source/graphics/camera.d
@@ -0,0 +1,15 @@
+﻿module graphics.camera;
+import math.matrix;
+import graphics.GraphicsState;
+
+public struct camera
+{
+	public mat4 projMatrix;
+	public mat4 viewMatrix;
+
+	public this(float fov, float aspect)
+	{
+		projMatrix = projectionMatrix(fov, aspect, -1.0f, -100.0f);
+		viewMatrix = translationMatrix(0.0f, 0.0f, 0.0f);
+	}
+}
