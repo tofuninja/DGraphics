a97f440595d60393a2a5ba9b919f6d035d2b80c2
Joseph Emmons <joeyemmons@yahoo.com>
2014-09-28 06:59:19 -0400

    Added lerp and toSlice
    I am wondering if this file makes sense or should be renamed or split up

--- source/math/conversion.d
+++ source/math/conversion.d
@@ -8,3 +8,17 @@ float toRad(float d)
 	import std.math;
 	return (d*PI)/180.0f;
 }
+
+// not really a math function but def a conversion function
+/**
+ * Convert a local to a slice of size 1
+ */
+auto toSlice(T)(ref T x)
+{
+	return (&x)[0..1];
+}
+
+auto lerp(float a, float b, float p)
+{
+	return a + (b-a)*p;
+}
