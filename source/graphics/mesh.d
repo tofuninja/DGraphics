e7957daee8b5cde6e18bd7a0535114e4a1d1808f
Joseph Emmons <joeyemmons@yahoo.com>
2015-02-17 19:05:05 -0500

    Reworked mesh to better work with opengl
    Now keepts track of its own buffers.
    Auto scales down
    Attach a mesh to a shader with a pre-defined mesh vertex interface

--- source/graphics/mesh.d
+++ source/graphics/mesh.d
@@ -3,77 +3,132 @@ import math.matrix;
 import graphics.color;
 import graphics.camera;
 import graphics.image;
+import graphics.hw.shader;
+import graphics.hw.buffer;
+
+// TODO: turn model into a carrier system for shader inputs + uniforms.... 
+// Basicly what I am thinking of is a way to combine a set of vertex data with a set of uniforms... 
 
 
+/*
+ * A model in world space
+ * Has a shaderInput(usually obtained from a mesh), texture, and a modelMatrix
+ * 
+ */
 public struct model
 {
 	// Rotation, translation and scaling are all handeled by the model matrix
 	public mat4 modelMatrix;
-	public mesh meshData;
+	public ShaderInput shaderInput;
 	public Image texture; 
 
-	public this(mesh meshDat)
+	public this(ShaderInput input)
 	{
-		meshData = meshDat;
+		shaderInput = input;
 		modelMatrix = identity!4;
 		texture = null;
 	}
 
-	public this(mesh meshDat, Image tex)
+	public this(ShaderInput input, Image tex)
 	{
-		this(meshDat);
+		this(input);
 		texture = tex;
 	}
 }
 
+/*
+ * Represents a mesh of triangles
+ * 
+ */
 public class mesh
 {
 	public vec3[] vectors;
-	public Color[] colors;
-	public vec3[] vcolors;
+	public vec3[] colors;
 	public vec3[] normals;
 	public vec2[] texCords;
 	public uvec3[] indices;
 
-	private vec3 lowerBound;
-	private vec3 upperBound;
-	private vec3 center;
+	public VertexBuffer!vec3 vectorsBuffer;
+	public VertexBuffer!vec3 colorsBuffer;
+	public VertexBuffer!vec3 normalsBuffer;
+	public VertexBuffer!vec2 texCordsBuffer;
+	public IndexBuffer indicesBuffer;
 
-	private this()
-	{
-		// do nothing
-	}
+	public bool buffersGenerated = false;
 
-	this(vec3[] vecs, uvec3[] index)
-	{
-		this(vecs, index, Color(255,255,255,255));
-	}
-
-	this(vec3[] vecs, uvec3[] index, Color c)
+	public this(vec3[] vecs, vec3[] cols, vec3[] norms, vec2[] tex, uvec3[] index)
 	{
 		vectors = vecs;
+		colors = cols;
+		normals = norms;
+		texCords = tex;
 		indices = index;
-		colors = new Color[vecs.length];
-		colors[] = c;
-		calcBounds();
-	}
 
-	private void calcBounds()
-	{
-		import std.algorithm;
-		lowerBound = vectors[0];
-		upperBound = vectors[0];
-		foreach(vec3 v; vectors)
+
+		vec3 lowerBound;
+		vec3 upperBound;
+		vec3 center;
+
+		void calcBounds()
 		{
-			lowerBound.x = min(lowerBound.x, v.x);
-			lowerBound.y = min(lowerBound.y, v.y);
-			lowerBound.z = min(lowerBound.z, v.z);
-			upperBound.x = max(upperBound.x, v.x);
-			upperBound.y = max(upperBound.y, v.y);
-			upperBound.z = max(upperBound.z, v.z);
+			import std.algorithm;
+			lowerBound = vectors[0];
+			upperBound = vectors[0];
+			foreach(vec3 v; vectors)
+			{
+				lowerBound.x = min(lowerBound.x, v.x);
+				lowerBound.y = min(lowerBound.y, v.y);
+				lowerBound.z = min(lowerBound.z, v.z);
+				upperBound.x = max(upperBound.x, v.x);
+				upperBound.y = max(upperBound.y, v.y);
+				upperBound.z = max(upperBound.z, v.z);
+			}
+			
+			center = (upperBound + lowerBound)/2.0f;
 		}
 
-		center = (upperBound + lowerBound)/2.0f;
+		void normilizeME()
+		{
+			auto size = (upperBound - lowerBound)/2;
+			for(int i = 0; i < vectors.length; i++)
+			{
+				vectors[i] = (vectors[i] - center)/size;
+			}
+			center = vec3(0,0,0);
+			lowerBound = vec3(-1,-1,-1);
+			lowerBound = vec3(1,1,1);
+		}
+
+		calcBounds();
+		normilizeME();
+	}
+
+	// Generate vertex buffers
+	public void generateBuffers()
+	{
+		if(buffersGenerated) deleteBuffers();
+
+		vectorsBuffer = VertexBuffer!vec3(vectors, "pos");
+		colorsBuffer = VertexBuffer!vec3(colors, "col");
+		normalsBuffer = VertexBuffer!vec3(normals, "norm");
+		texCordsBuffer = VertexBuffer!vec2(texCords, "uv");
+		indicesBuffer = IndexBuffer(indices);
+
+		buffersGenerated = true;
+	}
+
+	// Delete vertex buffers
+	public void deleteBuffers()
+	{
+		if(!buffersGenerated) return;
+
+		vectorsBuffer.destroy();
+		colorsBuffer.destroy();
+		normalsBuffer.destroy();
+		texCordsBuffer.destroy();
+		indicesBuffer.destroy();
+
+		buffersGenerated = false;
 	}
 }
 
@@ -83,6 +138,12 @@ public class mesh
 public mesh boxMesh()
 {
 	vec3[] vecs = new vec3[8];
+	vec3[] vcols = new vec3[8];
+	vec3[] norms = new vec3[8];
+	vec2[] uvArr = new vec2[8];
+	uvec3[] index = new uvec3[6*2];
+
+	// box verts 
 	vecs[0] = vec3(-1, 1, 1);
 	vecs[1] = vec3( 1, 1, 1);
 	vecs[2] = vec3(-1,-1, 1);
@@ -92,8 +153,28 @@ public mesh boxMesh()
 	vecs[6] = vec3(-1,-1,-1);
 	vecs[7] = vec3( 1,-1,-1);
 
-	uvec3[] index = new uvec3[6*2];
+	// box colors
+	vcols[] = vec3(1,1,1);
 
+
+	// box normals
+	for(int i = 0; i < 8; i++)
+	{
+		norms[i] = normalize(vecs[i]);
+	}
+
+	// box texCords
+	enum scale = 1;
+	uvArr[0] = vec2(0,0);
+	uvArr[1] = vec2(scale,0);
+	uvArr[2] = vec2(0,scale);
+	uvArr[3] = vec2(scale,scale);
+	uvArr[4] = vec2(scale,scale);
+	uvArr[5] = vec2(0,scale);
+	uvArr[6] = vec2(scale,0);
+	uvArr[7] = vec2(0,0);
+
+	// box indecies
 	index[0] = uvec3(0,2,3);
 	index[1] = uvec3(3,1,0);
 	index[2] = uvec3(1,3,7);
@@ -106,39 +187,12 @@ public mesh boxMesh()
 	index[9] = uvec3(6,2,0);
 	index[10] = uvec3(6,7,2);
 	index[11] = uvec3(2,7,3);
-	mesh m = new mesh(vecs, index);
 
-	vec3[] norms = new vec3[8];
-	for(int i = 0; i < 8; i++)
-	{
-		norms[i] = normalize(vecs[i]);
-	}
-	m.normals = norms;
-
-	Color[] cols = new Color[8];
-	cols[] = Color(255,255,255,255);
-	m.colors = cols;
-
-	vec3[] vcols = new vec3[8];
-	vcols[] = vec3(1,1,1);
-	m.vcolors = vcols;
-
-	enum scale = 1;
-	vec2[] uvArr = new vec2[8];
-	uvArr[0] = vec2(0,0);
-	uvArr[1] = vec2(scale,0);
-	uvArr[2] = vec2(0,scale);
-	uvArr[3] = vec2(scale,scale);
-	uvArr[4] = vec2(scale,scale);
-	uvArr[5] = vec2(0,scale);
-	uvArr[6] = vec2(scale,0);
-	uvArr[7] = vec2(0,0);
-
-	m.texCords = uvArr;
-
-	return m;
+	return new mesh(vecs, vcols, norms, uvArr, index);
 }
 
+// Todo: fix spere and cylinder mesh generators
+/*
 public mesh sphereMesh()
 {
 	import std.math;
@@ -251,105 +305,11 @@ mesh cylinderMesh()
 
 	return new mesh(vecs, index);
 }
-
-// Replaced by runShader
-/*
-public void draw(alias vertShader, alias pixShader)(Image img, model m, camera c, light l)
-{
-	import graphics.render;
-	auto mm = m.modelMatrix;
-	auto vp = c.projMatrix*c.viewMatrix;
-	
-	// No conectivity data, just render as a point cloud 
-	if(m.meshData.indices is null)
-	{
-		drawPoints(img, m, c);
-		return;
-	}
-	
-	foreach(uvec3 tri; m.meshData.indices)
-	{
-		vec4 p0;
-		vec4 p1;
-		vec4 p2;
-		
-		p0.xyz = m.meshData.vectors[tri[0]];
-		p1.xyz = m.meshData.vectors[tri[1]];
-		p2.xyz = m.meshData.vectors[tri[2]];
-		
-		p0.w = 1;
-		p1.w = 1;
-		p2.w = 1;
-
-		p0 = mm*p0;
-		p1 = mm*p1;
-		p2 = mm*p2;
-
-		Color c1,c2,c3;
-
-		if(l.mode == 1 && m.meshData.normals !is null)
-		{
-			import std.algorithm;
-			c1 = /*l.col*l.amb +* / l.col*min(1,l.dif*max(0,dot(m.meshData.normals[tri[0]], p0.xyz - l.loc)));
-			c2 = /*l.col*l.amb +* / l.col*min(1,l.dif*max(0,dot(m.meshData.normals[tri[1]], p1.xyz - l.loc)));
-			c3 = /*l.col*l.amb +* / l.col*min(1,l.dif*max(0,dot(m.meshData.normals[tri[2]], p2.xyz - l.loc)));
-		}
-		
-		p0 = vp*p0;
-		p1 = vp*p1;
-		p2 = vp*p2;
-		
-		p0 = p0 / p0.w;
-		p1 = p1 / p1.w;
-		p2 = p2 / p2.w;
-		
-		auto size = vec2(img.Width, img.Height);
-		size = size/2.0f;
-
-		/*
-		void plotTri(vec4 v0, vec4 v1, vec4 v2, Color col)
-		{
-
-		}
-
-		plotLine(p0,p1,(m.meshData.colors !is null) ? m.meshData.colors[tri[0]] : Color(255,255,255,255));
-		plotLine(p1,p2,(m.meshData.colors !is null)? m.meshData.colors[tri[1]] : Color(255,255,255,255));
-		plotLine(p2,p0,(m.meshData.colors !is null) ? m.meshData.colors[tri[2]] : Color(255,255,255,255));
-		* /
-
-		import util.debugger;
-		import std.stdio;
-
-
-		if((p0.z > -1 && p0.z < 1) && (p1.z > -1 && p1.z < 1) && (p2.z > -1 && p2.z < 1))
-		{
-			//img.drawLine(v0.xy*size + size, v1.xy*size + size, col);
-			vec3 A = p0.xyz;
-			vec3 B = p1.xyz;
-			vec3 C = p2.xyz;
-			A.xy = A.xy*size + size;
-			B.xy = B.xy*size + size;
-			C.xy = C.xy*size + size;
-			foreach(vec3 point, vec3 p; triangleRaster3D(A, B, C))
-			{
-				Color col = Color(255,255,255,255);
-				if(l.mode == 0)
-				{
-					if(m.meshData.colors !is null) col = m.meshData.colors[tri[0]]*p.x + m.meshData.colors[tri[1]]*p.y + m.meshData.colors[tri[2]]*p.z;
-				}
-				else if(l.mode == 1)
-				{
-					col = Color(cast(byte)(255*(point.z + 2)),0,0);//c1*p.x + c2*p.y + c3*p.z;
-				}
-				//writeln(point.z);
-				//breakPoint();
-				img[point] = col;
-			}
-		}
-	}
-}
 */
 
+
+// TODO: fix it or remove it, model no longer works how this expects it... maybe there needs to be a seperation between a hw model and a sw model... 
+/*
 public void drawWireModel(Image img, model m, camera c)
 {
 	import graphics.render;
@@ -403,9 +363,9 @@ public void drawWireModel(Image img, model m, camera c)
 			}
 		}
 
-		plotLine(p0,p1,(m.meshData.colors !is null) ? m.meshData.colors[tri[0]] : Color(255,255,255,255));
-		plotLine(p1,p2,(m.meshData.colors !is null)? m.meshData.colors[tri[1]] : Color(255,255,255,255));
-		plotLine(p2,p0,(m.meshData.colors !is null) ? m.meshData.colors[tri[2]] : Color(255,255,255,255));
+		plotLine(p0,p1,(m.meshData.colors !is null) ? m.meshData.colors[tri[0]].to!Color : Color(255,255,255,255));
+		plotLine(p1,p2,(m.meshData.colors !is null)? m.meshData.colors[tri[1]].to!Color : Color(255,255,255,255));
+		plotLine(p2,p0,(m.meshData.colors !is null) ? m.meshData.colors[tri[2]].to!Color : Color(255,255,255,255));
 	}
 }
 
@@ -422,10 +382,10 @@ public void drawPoints(Image img, model m, camera c)
 		p0 = mvp*p0;
 		p0 = p0 / p0.w;
 
-		img[p0.xyz] = (m.meshData.colors !is null) ? m.meshData.colors[i] : Color(255,255,255,255);
+		img[p0.xyz] = (m.meshData.colors !is null) ? m.meshData.colors[i].to!Color : Color(255,255,255,255);
 	}
 }
-
+*/
 void drawFrustrum(Image img, mat4 m, camera c)
 {
 	import graphics.render;
@@ -494,6 +454,7 @@ void drawFrustrum(Image img, mat4 m, camera c)
 	}
 }
 
+
 public void setModelMatrix(ref model m,vec3 translation, vec3 rotation, vec3 scale)
 {
 	m.modelMatrix = translationMatrix(translation)*rotationMatrix(rotation)*scalingMatrix(scale);
@@ -534,69 +495,42 @@ mesh loadMesh(string file)
 
 	// Reserve space
 	verts = new vec3[vertsN];
-	if(hasCols == 'y')cols = new vec3[vertsN];
-	if(hasNorms == 'y') norms = new vec3[vertsN];
-	if(hasTexts == 'y') texCords = new vec2[vertsN];
+	cols = new vec3[vertsN];
+	norms = new vec3[vertsN];
+	texCords = new vec2[vertsN];
 
 	// Read Data
 	f.rawRead(verts);
-	if(cols) f.rawRead(cols);
-	if(norms) f.rawRead(norms);
-	if(texCords) f.rawRead(texCords);
+	if(hasCols  == 'y') f.rawRead(cols); 		else cols[] 	= vec3(255,255,255);
+	if(hasNorms == 'y') f.rawRead(norms); 		else norms[] 	= vec3(0,0,0);
+	if(hasTexts == 'y') f.rawRead(texCords); 	else texCords[] = vec2(0,0);
 
 	// Read indicies 
 	f.rawRead(trisN.toSlice);
 	tris = new uvec3[trisN];
 	f.rawRead(tris);
 
-	// Convert Colors
-	Color[] colsConverted;
-	if(cols)
-	{
-		colsConverted = new Color[vertsN];
-
-		for(int i = 0; i < vertsN; i++)
-		{
-			vec4 c;
-			c.xyz = cols[i];
-			c.w = 1;
-			colsConverted[i] = (c*255).to!Color;
-		}
-	}
-
-	// Set up rtn
-	mesh rtn = new mesh();
-	rtn.vectors = verts;
-	rtn.colors = colsConverted;
-	rtn.vcolors = cols;
-	rtn.normals = norms;
-	rtn.texCords = texCords;
-	rtn.indices = tris;
-	rtn.calcBounds();
-
-	static if(false)
-	{
-		writeln("VertCount:",vertsN);
-		writeln("HasVerts:",hasVerts);
-		writeln("HasCols:",hasCols);
-		writeln("HasNorms:",hasNorms);
-		writeln("HasTextCords:",hasTexts);
-
-		writeln("Center:",rtn.center);
-		writeln("LowerBound:",rtn.lowerBound);
-		writeln("UpperBound:",rtn.upperBound);
-	}
-	return rtn;
+	return new mesh(verts, cols, norms, texCords, tris);
 }
 
-void normilize(mesh m)
+
+
+
+
+/*
+ * Attach a mesh to a shader input
+ * Assumes the shader as the standard mesh interface as follows
+ * 		vec3 pos
+ * 		vec3 col
+ * 		vec3 norm
+ * 		vec2 uv
+ */
+public void attachMesh(ref ShaderInput input, mesh m)
 {
-	auto size = (m.upperBound - m.lowerBound)/2;
-	for(int i = 0; i < m.vectors.length; i++)
-	{
-		m.vectors[i] = (m.vectors[i] - m.center)/size;
-	}
-	m.center = vec3(0,0,0);
-	m.lowerBound = vec3(-1,-1,-1);
-	m.lowerBound = vec3(1,1,1);
+	if(!m.buffersGenerated) m.generateBuffers();
+	input.attachBuffer(m.vectorsBuffer);
+	input.attachBuffer(m.colorsBuffer);
+	input.attachBuffer(m.normalsBuffer);
+	input.attachBuffer(m.texCordsBuffer);
+	input.attachIndexBuffer(m.indicesBuffer);
 }
