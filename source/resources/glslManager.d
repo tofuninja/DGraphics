4a6496dcec530023d939dfb3a8d3d01cda1ca4ac
Joseph Emmons <joeyemmons@yahoo.com>
2015-02-17 19:06:07 -0500

    GLSL manager: Keep track of glsl files

---
+++ source/resources/glslManager.d
@@ -0,0 +1,9 @@
+﻿module resources.glslManager;
+
+// For now, just stacicly include the glsl code into the program
+// Maybe this could be changed later to load at run time but I dont really see a benifit
+
+
+// All text imports are located in the views folder
+public enum simpleShaderVert = import("simpleShader.vertex.glsl");
+public enum simpleShaderFrag = import("simpleShader.fragment.glsl");
