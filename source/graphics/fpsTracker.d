a6964f29a733da0f26843711f7af8069f9ef8926
Joseph Emmons <joeyemmons@yahoo.com>
2015-02-17 19:00:18 -0500

    Simple fps tracker

---
+++ source/graphics/fpsTracker.d
@@ -0,0 +1,32 @@
+﻿module graphics.fpsTracker;
+import std.datetime;
+
+auto FPSTracker()
+{
+	struct fpsTracker
+	{
+		private int frame = 0;
+		private SysTime lastTime;
+		public real fps = 0;
+		public int totalFrames = 0;
+		
+		public this(SysTime now)
+		{
+			lastTime = now;
+		}
+		
+		public void postFrame()
+		{
+			frame++;
+			totalFrames ++;
+			if((Clock.currTime - lastTime).total!"msecs" > 1000)
+			{
+				fps = frame*(1000.0/(Clock.currTime - lastTime).total!"msecs");
+				lastTime = Clock.currTime;
+				frame = 0;
+			}
+		}
+	}
+	return fpsTracker(Clock.currTime);
+}
+
